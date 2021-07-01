b3eb0b412a59d5a905bd1a3c6bea019a4fe9eb53
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-07-01 15:07:00 +0530

    Computing Power Of Two Program

---
+++ Basicprograms/PowerOfTwo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basicprograms
+{
+    class PowerOfTwo
+    {
+       
+            public static void ReadInput()
+            {
+
+                Console.WriteLine("Enter Power : ");
+                int Power = Convert.ToInt32(Console.ReadLine());
+                FindPowerOfTwo(Power);
+            }
+        public static void FindPowerOfTwo(int num)
+        {
+            
+            for(int i=0;i<=num;i++)
+            {
+                Console.WriteLine(Math.Pow(2,i)+" ");
+            }
+
+        }
+     }
+}
--- Basicprograms/Program.cs
+++ Basicprograms/Program.cs
@@ -8,19 +8,26 @@ namespace Basicprograms
         {
             Console.WriteLine("Implementation of basic programs");
             Console.WriteLine("Enter choice to select programs: ");
-            Console.WriteLine("1.Flip Coin 2.Leap Year");
+            Console.WriteLine("1.Flip Coin 2.Leap Year 3. Power Of Two");
             int choice = Convert.ToInt32(Console.ReadLine());
+            //Selection statement
             switch(choice)
             {
                 case 1:
+                    //Computing Flip Coin Program
                     Console.WriteLine("Flip Coin Program!");
                     FlipCoin.ReadInput();
                     break;
                 case 2:
+                    ////Computing Leap Year Program
                     Console.WriteLine("Finding Leap Year Or Not!");
                     LeapYear.ReadInput();
                     break;
-
+                case 3:
+                    ////Computing Power Of Two Program
+                    Console.WriteLine("Finding Power Of Two");
+                    PowerOfTwo.ReadInput();
+                    break;
             }
           
         }
