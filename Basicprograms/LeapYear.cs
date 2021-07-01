50e92b3f2810579cb603d275335d569801afb977
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-07-01 14:51:37 +0530

    Leap Year Program

---
+++ Basicprograms/LeapYear.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basicprograms
+{
+    class LeapYear
+    {
+        public static void ReadInput()
+        {
+         
+            Console.WriteLine("Enter Year : ");
+            int Year = Convert.ToInt32(Console.ReadLine());
+            FindLeapYear(Year);
+        }
+        public static void FindLeapYear(int Year)
+        {
+
+            
+            if (((Year % 4 == 0) && (Year % 100 != 0)) || (Year % 400 == 0))
+            {
+                Console.WriteLine("{0} is a Leap Year.", Year);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a Leap Year.", Year);
+            }
+
+        }           
+    }
+}
--- Basicprograms/Program.cs
+++ Basicprograms/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
 
-namespace ConsoleApp1
+namespace Basicprograms
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Implementation of basic programs");
-            Console.WriteLine("Flip Coin Program!");
-            //Calling Flipcoin method using ReadInput...
-            FlipCoin.ReadInput();
+            Console.WriteLine("Enter choice to select programs: ");
+            Console.WriteLine("1.Flip Coin 2.Leap Year");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch(choice)
+            {
+                case 1:
+                    Console.WriteLine("Flip Coin Program!");
+                    FlipCoin.ReadInput();
+                    break;
+                case 2:
+                    Console.WriteLine("Finding Leap Year Or Not!");
+                    LeapYear.ReadInput();
+                    break;
+
+            }
+          
         }
     }
 }
